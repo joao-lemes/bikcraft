d10e9134dff9f408740b473be8cfc445a76f6680
João Gabriel <joao.lemes.info@gmail.com>
2022-10-13 16:39:17 -0300

    Function on often questions

--- js/script.js
+++ js/script.js
@@ -27,3 +27,21 @@ function activeProducts(item) {
         budgetProduct.checked = true;
     }
 }
+
+// Often Questions
+const questions = document.querySelectorAll('.questions button');
+
+questions.forEach(questionsEvents);
+
+function questionsEvents(item) {
+    item.addEventListener('click', activeQuestions);
+}
+
+function activeQuestions(e) {
+    const question = e.currentTarget;
+    const controls = question.getAttribute('aria-controls');
+    const answer = document.querySelector('#' + controls);
+
+    answer.classList.toggle('actived');
+    question.setAttribute('aria-expanded', answer.classList.contains('actived'));
+}
